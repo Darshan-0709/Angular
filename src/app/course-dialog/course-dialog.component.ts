38306149c5b36b85ef3c0cba09543457a54c8721
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2025-05-16 07:10:24 +0530

    feat: couser service, and smart presentation component with loading component ;

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,51 +1,63 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import moment from 'moment';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import moment from "moment";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
+import { CoursesService } from "../services/courses.service";
+import { LoadingService } from "../loading/loading.service";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css'],
-    standalone: false
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
+  standalone: false,
+  providers: [LoadingService]
 })
 export class CourseDialogComponent implements AfterViewInit {
+  form: FormGroup;
 
-    form: FormGroup;
+  course: Course;
 
-    course:Course;
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course,
+    private coursesService: CoursesService,
+    private loadingService: LoadingService
+  ) {
+    this.course = course;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course) {
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required],
+    });
+  }
 
-        this.course = course;
+  ngAfterViewInit() {}
 
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
-        });
+  save() {
+    const changes = this.form.value;
+    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes) 
 
-    }
+    this.loadingService.showLoaderUntilComplete(saveCourse$)
+      .subscribe((value) => {
+        this.dialogRef.close(value);
+      });
+  }
 
-    ngAfterViewInit() {
-
-    }
-
-    save() {
-
-      const changes = this.form.value;
-
-    }
-
-    close() {
-        this.dialogRef.close();
-    }
-
+  close() {
+    this.dialogRef.close();
+  }
 }
